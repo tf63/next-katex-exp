33d510e5a92b6896e9f1b05a2e6036fc6d668fd2
tf63 <74246282+tf63@users.noreply.github.com>
2024-07-15 19:39:06 +0900

    [feat] impl katex input component

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     return (
         <div className="h-screen p-10">
             <h1 className="text-2xl font-bold">Math Input</h1>
-            <div className="w-full py-5">
+            <div className="my-10 w-full">
                 <MathInput />
             </div>
         </div>
--- components/math-input/math-input.tsx
+++ components/math-input/math-input.tsx
@@ -1,27 +1,42 @@
 'use client'
 
 import katex from 'katex'
-import { useEffect, useRef } from 'react'
+import { ChangeEvent, useEffect, useRef, useState } from 'react'
 import 'katex/dist/katex.min.css'
 
 export const MathInput = () => {
+    const [formula, setFormula] = useState<string>('')
     const formulaRef = useRef<HTMLDivElement>(null)
 
     useEffect(() => {
-        // 数式をHTML要素にレンダリング
         const formulaElement = formulaRef.current
 
         if (formulaElement == null) {
             return
         }
-        katex.render('c = \\pm\\sqrt{a^2 + b^2}', formulaElement, {
+        katex.render(formula, formulaElement, {
             throwOnError: false,
         })
-    }, [])
+    }, [formula])
+
+    const onChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
+        const value = event.target.value
+        setFormula(value)
+    }
 
     return (
-        <div>
-            <div ref={formulaRef}></div>
+        <div className="w-full">
+            <div>
+                <span ref={formulaRef} className="inline-block" />
+            </div>
+
+            <textarea
+                value={formula}
+                onChange={onChange}
+                spellCheck={false}
+                className="textarea textarea-bordered my-5 h-60 w-full"
+                placeholder="Input formula..."
+            />
         </div>
     )
 }
