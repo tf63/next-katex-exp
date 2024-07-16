113a1d25d307400a14082eea105c405d97b19527
tf63 <74246282+tf63@users.noreply.github.com>
2024-07-16 12:37:37 +0900

    [feat] impl formula dropdown

---
+++ components/formula-dropdown/formula-dropdown.spec.tsx
@@ -0,0 +1,6 @@
+import { render, screen } from '@testing-library/react'
+import { FormulaDropdown } from '.'
+
+test('renders FormulaDropdown component', () => {
+    render(<FormulaDropdown />)
+})
