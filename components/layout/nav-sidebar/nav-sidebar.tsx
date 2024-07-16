d22d8675110b8ff526cec886f8ceac426d8b2c14
tf63 <74246282+tf63@users.noreply.github.com>
2024-07-16 16:26:03 +0900

    [feat] make sidebar responsive

--- components/layout/nav-sidebar/nav-sidebar.tsx
+++ components/layout/nav-sidebar/nav-sidebar.tsx
@@ -1,8 +1,15 @@
+import { Formula } from '@/components/formula'
+
+import { CopyBlock } from '../copy-block'
+
 export const NavSidebar = () => {
     return (
-        <div className="w-full rounded-md bg-neutral p-4">
+        <div className="my-5 h-full w-full overflow-scroll rounded-md bg-neutral p-4">
             <h2 className="mb-5 text-center text-xl font-semibold">Templates</h2>
             <p className="font-semibold">Environment</p>
+            <CopyBlock text="\bm x = \bm a_0 + \bm b_0">
+                <Formula inline={true} formula="\bm x = \bm a_0 + \bm b_0" />
+            </CopyBlock>
             <div className="divider" />
             <p className="font-semibold">Align</p>
             <div className="divider" />
