a75223eab5e1ad638b8101c068c5810488099f9b
tf63 <74246282+tf63@users.noreply.github.com>
2024-07-16 19:18:54 +0900

    [feat] setup tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,15 @@ const config = {
     ],
     theme: {
         extend: {
+            backgroundImage: {
+                grad: 'linear-gradient(315deg, #DE94FF 0%, #A1B1FF 100%)',
+            },
+            boxShadow: {
+                sm: '4px 10px 40px rgba(34, 26, 53, 0.05)',
+                md: '4px 10px 40px rgba(34, 26, 53, 0.06)',
+                lg: '4px 10px 40px rgba(34, 26, 53, 0.07)',
+                grad: '4px 10px 20px rgba(219, 150, 255, 0.4);',
+            },
             keyframes: {
                 blink: {
                     '0%, 100%': { opacity: '1' },
@@ -19,14 +28,25 @@ const config = {
             },
         },
     },
-    plugins: [require('daisyui')],
+    plugins: [
+        require('daisyui'),
+
+        function ({ addUtilities }) {
+            const newUtilities = {
+                '.h-sidebar': {
+                    height: 'calc(100vh - 25px)',
+                },
+            }
+            addUtilities(newUtilities, ['responsive', 'hover'])
+        },
+    ],
     daisyui: {
         themes: [
             'light',
             {
                 emerald: {
                     ...require('daisyui/src/theming/themes')['emerald'],
-                    '--primary': '#ffffff',
+                    'base-200': '#fcfcfc',
                 },
             },
             'dark',
