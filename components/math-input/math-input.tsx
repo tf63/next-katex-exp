0bb3fffbae0c401d6d32c286e904475ad15fe9e5
tf63 <74246282+tf63@users.noreply.github.com>
2024-07-16 13:30:41 +0900

    [feat] extract formula component

--- components/math-input/math-input.tsx
+++ components/math-input/math-input.tsx
@@ -1,33 +1,21 @@
 'use client'
 
-import katex from 'katex'
-import { ChangeEvent, useRef, useState } from 'react'
+import { ChangeEvent, useState } from 'react'
+
 import 'katex/dist/katex.min.css'
+import { Formula } from '../formula'
 
 export const MathInput = ({ inline }: { inline: boolean }) => {
     const [formula, setFormula] = useState<string>('')
-    const formulaRef = useRef<HTMLDivElement>(null)
 
     const onChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
         const value = event.target.value
         setFormula(value)
-
-        const formulaElement = formulaRef.current
-
-        if (formulaElement == null) {
-            return
-        }
-        katex.render(value, formulaElement, {
-            throwOnError: false,
-            displayMode: !inline,
-        })
     }
 
     return (
         <div className="w-full">
-            <div className="h-14">
-                <span ref={formulaRef} className="inline-block" />
-            </div>
+            <Formula formula={formula} inline={inline} />
 
             <textarea
                 value={formula}
