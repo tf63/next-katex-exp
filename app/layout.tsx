22b3dafcae7ff05d599aa6013ece15f31a39cdc7
tf63 <74246282+tf63@users.noreply.github.com>
2024-07-16 12:28:42 +0900

    [feat] configure theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
     children: React.ReactNode
 }>) {
     return (
-        <html lang="ja">
+        <html lang="ja" data-theme="emerald">
             <body className={inter.className}>{children}</body>
         </html>
     )
