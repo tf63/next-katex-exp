20d72eb74fce42ae57eaaae277b5bea1ad6f6335
tf63 <74246282+tf63@users.noreply.github.com>
2024-07-17 22:19:01 +0900

    [feat] impl color picker

--- components/formula-input/formula-input.tsx
+++ components/formula-input/formula-input.tsx
@@ -1,16 +1,22 @@
 'use client'
 
 import { ChangeEvent, useRef, useState } from 'react'
+import { Alpha, Hue, Saturation, useColor } from 'react-color-palette'
 
 import { useDOMtoImage } from '@/hooks/use-dom-to-image'
 
 import { Formula } from '@/components/formula'
+
+import 'react-color-palette/css'
+
 type FormulaInputProps = {
     inline: boolean
 }
 
 export const FormulaInput = ({ inline }: FormulaInputProps) => {
     const [formula, setFormula] = useState<string>('')
+
+    const [color, setColor] = useColor('#000000FF')
 
     const elementRef = useRef<HTMLDivElement>(null)
 
@@ -23,7 +29,7 @@ export const FormulaInput = ({ inline }: FormulaInputProps) => {
 
     return (
         <div className="w-full">
-            <div className="mx-20 border-b-2 py-2">
+            <div className="mx-20 border-b-2 py-2" style={{ color: color.hex }}>
                 <Formula ref={elementRef} formula={formula} inline={inline} />
             </div>
             <textarea
@@ -41,6 +47,33 @@ export const FormulaInput = ({ inline }: FormulaInputProps) => {
                 <button className="btn btn-secondary shadow-secondary outline-none" onClick={copyImage} tabIndex={-1}>
                     Copy as Image
                 </button>
+
+                {/* <dialog id="color-picker" className="modal">
+                    <div className="modal-box w-fit rounded-lg bg-base-100 shadow-lg">
+                        <div className="w-[100px] space-y-10 [&_.rcp-saturation]:h-[100px] [&_.rcp-saturation]:w-[100px] [&_.rcp-saturation]:rounded-md">
+                            <Saturation height={100} color={color} onChange={setColor} />
+                            <Hue color={color} onChange={setColor} />
+                            <Alpha color={color} onChange={setColor} />
+                        </div>
+                    </div>
+
+                    <form method="dialog" className="modal-backdrop">
+                        <button>close</button>
+                    </form>
+                </dialog> */}
+
+                <details className="dropdown dropdown-top">
+                    <summary className="btn min-w-[100px] font-normal shadow-primary hover:bg-base-100/90">
+                        {color.hex}
+                    </summary>
+                    <div className="dropdown-content z-auto mb-5 rounded-lg bg-base-100 p-5 shadow-lg">
+                        <div className="space-y-5 [&_.rcp-saturation]:h-[200px] [&_.rcp-saturation]:w-[200px] [&_.rcp-saturation]:rounded-md">
+                            <Saturation height={200} color={color} onChange={setColor} />
+                            <Hue color={color} onChange={setColor} />
+                            <Alpha color={color} onChange={setColor} />
+                        </div>
+                    </div>
+                </details>
             </div>
         </div>
     )
