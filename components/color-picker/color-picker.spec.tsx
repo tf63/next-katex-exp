b5314ab45ea973527271b1e50af3bcde5268753f
tf63 <74246282+tf63@users.noreply.github.com>
2024-07-17 22:53:34 +0900

    [feat] extract color picker components

---
+++ components/color-picker/color-picker.spec.tsx
@@ -0,0 +1,6 @@
+import { render, screen } from '@testing-library/react'
+import { ColorPicker } from '.'
+
+test('renders ColorPicker component', () => {
+    render(<ColorPicker />)
+})
--- components/formula-input/formula-input.tsx
+++ components/formula-input/formula-input.tsx
@@ -1,11 +1,11 @@
 'use client'
 
 import { ChangeEvent, useRef, useState } from 'react'
-import { Alpha, Hue, Saturation, useColor } from 'react-color-palette'
+import { useColor } from 'react-color-palette'
 
-import { useCloseRef } from '@/hooks/use-close-ref'
 import { useDOMtoImage } from '@/hooks/use-dom-to-image'
 
+import { ColorPicker } from '@/components/color-picker'
 import { Formula } from '@/components/formula'
 
 import 'react-color-palette/css'
@@ -16,7 +16,6 @@ type FormulaInputProps = {
 
 export const FormulaInput = ({ inline }: FormulaInputProps) => {
     const [formula, setFormula] = useState<string>('')
-    const { closeRef } = useCloseRef()
 
     const [color, setColor] = useColor('#000000FF')
 
@@ -49,19 +48,7 @@ export const FormulaInput = ({ inline }: FormulaInputProps) => {
                 <button className="btn btn-secondary shadow-secondary outline-none" onClick={copyImage} tabIndex={-1}>
                     Copy as Image
                 </button>
-
-                <details className="dropdown dropdown-top" ref={closeRef}>
-                    <summary className="shadow-btn btn min-w-[100px] font-semibold hover:border-transparent hover:bg-base-200">
-                        Text {color.hex}
-                    </summary>
-                    <div className="shadow-btn dropdown-content z-50 mb-5 rounded-lg bg-base-200 p-5">
-                        <div className="w-[175px] space-y-5 [&_.rcp-saturation]:rounded-md">
-                            <Saturation height={175} color={color} onChange={setColor} />
-                            <Hue color={color} onChange={setColor} />
-                            <Alpha color={color} onChange={setColor} />
-                        </div>
-                    </div>
-                </details>
+                <ColorPicker color={color} setColor={setColor} />
             </div>
         </div>
     )
