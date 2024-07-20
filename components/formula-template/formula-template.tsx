ab610e8b060593935478a4de3b98f04fa0e2cae5
tf63 <74246282+tf63@users.noreply.github.com>
2024-07-20 22:16:15 +0900

    [feat] add template number

--- components/formula-template/formula-template.tsx
+++ components/formula-template/formula-template.tsx
@@ -6,7 +6,12 @@ export const FormulaTemplate = ({ template }: { template: Template }) => {
 
     return (
         <CopyBlock text={formula}>
-            <Formula inline={false} formula={formula} />
+            <div className="relative w-full">
+                <div className="absolute right-2 top-0 rounded-md bg-slate-600 bg-opacity-15 px-1 py-0.5 text-[10px]">
+                    {template.kbd}
+                </div>
+                <Formula inline={false} formula={formula} />
+            </div>
         </CopyBlock>
     )
 }
